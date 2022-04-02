5289fe71bb6301ddc1fff16e1c372f108c40c754
paul pettipas <paul.pettipas@gmail.com>
2022-04-02 16:28:07 -0400

    player use nav move

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     public float Speed;
     public Transform Body;
+    public NavMeshAgent Agent;
 
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
         Vector3 dir = new Vector3(x,0,z);
-        transform.position += Speed * dir * Time.smoothDeltaTime;
+        Agent.Move(Speed * dir * Time.smoothDeltaTime);
 
         if(dir != Vector3.zero){
              Body.transform.forward = dir.normalized;
